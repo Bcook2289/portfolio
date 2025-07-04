80a0996fb68ec27fc1ad879a20206e6161742d83
bcook <bcook2289@gmail.com>
2025-07-03 22:32:24 -0400

    feat: changed project to typescript created the start of a basic skeleton

---
+++ portfolio-project/src/components/Hero.tsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello.</h1>
+        <h1>My name is Brandon</h1>
+        <h1>I'm a Software Developer</h1>
+      </div>
+      <div>Image</div>
+    </>
+  );
+};
+export default Hero;
---
+++ portfolio-project/src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+  return (
+    <>
+      <a href="">Home</a>
+      <a href="">About</a>
+      <a href="">Projects</a>
+      <a href="">Contact</a>
+    </>
+  );
+};
+export default NavBar;
