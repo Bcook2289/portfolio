f4d4d75c329272d8e024fed94ed088c948282793
Brandon Cook <bcook@ESGNA.COM>
2025-07-08 11:58:53 -0400

    feat basic work on Hero section

--- portfolio-project/src/components/Hero.tsx
+++ portfolio-project/src/components/Hero.tsx
@@ -1,12 +1,20 @@
+import Image from "next/image";
+
 const Hero = () => {
   return (
     <>
-      <div>
+      <div className="flex justify-center w-full p-4 m-4">
+        <Image 
+          src="/images/ProfilePic.jpg" 
+          alt="profile" 
+          width={200} 
+          height={200}
+          className="rounded-full object-cover border-4 m-1">
+        </Image>
         <h1>Hello.</h1>
-        <h1>My name is Brandon</h1>
-        <h1>I'm a Software Developer</h1>
+        <h2>My name is Brandon</h2>
+        <h2>I&apos;m a Software Developer</h2>
       </div>
-      <div>Image</div>
     </>
   );
 };
