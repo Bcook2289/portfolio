453a94a38c4c6fdf900791c9a4e1d0f83431c60d
Brandon Cook <bcook@ESGNA.COM>
2025-07-08 14:07:20 -0400

    basic styling for the image

--- portfolio-project/src/components/Hero.tsx
+++ portfolio-project/src/components/Hero.tsx
@@ -4,16 +4,21 @@ const Hero = () => {
   return (
     <>
       <div className="flex justify-center w-full p-4 m-4">
-        <Image 
-          src="/images/ProfilePic.jpg" 
-          alt="profile" 
-          width={200} 
-          height={200}
-          className="rounded-full object-cover border-4 m-1">
-        </Image>
-        <h1>Hello.</h1>
-        <h2>My name is Brandon</h2>
-        <h2>I&apos;m a Software Developer</h2>
+        <div className="relative w-40 h-40 p-2">
+          <Image 
+            src="/images/ProfilePic.jpg" 
+            alt="profile" 
+            width={200}
+            height={200}
+            className="rounded-full">
+          </Image>
+        </div>
+        <div className="p-4">
+          <h1>Hello.</h1>
+          <h2>My name is Brandon</h2>
+          <h2>I&apos;m a Software Developer</h2>
+          <button>Projects</button>
+        </div>
       </div>
     </>
   );
