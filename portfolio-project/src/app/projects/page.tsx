5ee52cd603a347c43a71a134726a2eb08ce417aa
Brandon Cook <bcook@ESGNA.COM>
2025-07-10 16:38:02 -0400

    implemented a data structure to prepare for the creation of project cards

--- portfolio-project/src/app/projects/page.tsx
+++ portfolio-project/src/app/projects/page.tsx
@@ -1,4 +1,22 @@
+import { m } from "framer-motion";
 import NavBar from "../../components/NavBar";
+import Image from "next/image";
+
+// data structure
+// project title
+// description
+// tech stack
+// image url
+// project link
+const projectData = [
+    {
+        title: "Hato",
+        desc: "A social media app designed around flashcards",
+        tech: ["Express.js", "Node.js", "React.js", "Knex.js", "PostgreSQL", "JavaScript", "Typescript"],
+        image: <><Image src={"/project-captures/Hato.JPG"} alt="Hato"/></>,
+        link: "https://github.com/hato-app"
+    }
+];
 
 export default function ProjectsPage() {
     return (
