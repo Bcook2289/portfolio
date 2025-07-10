729620a10ea2d945e64425f94095385ceb86d86d
Brandon Cook <bcook@ESGNA.COM>
2025-07-10 16:39:33 -0400

    fix: simple fix to avoid a bug with implementing images in the cards

--- portfolio-project/src/app/projects/page.tsx
+++ portfolio-project/src/app/projects/page.tsx
@@ -13,9 +13,31 @@ const projectData = [
         title: "Hato",
         desc: "A social media app designed around flashcards",
         tech: ["Express.js", "Node.js", "React.js", "Knex.js", "PostgreSQL", "JavaScript", "Typescript"],
-        image: <><Image src={"/project-captures/Hato.JPG"} alt="Hato"/></>,
+        image: "/project-captures/Hato.JPG",
         link: "https://github.com/hato-app"
-    }
+    },
+    {
+        title: "Dokushojo",
+        desc: "A Language Learning app focusing on listening comprehension",
+        tech: ["Express.js", "Node.js", "React.js", "Knex.js", "Bootstrap CSS", "PostgreSQL", "Vite", "Typescript"],
+        image: "/project-captures/DSJ.JPG",
+        link: "https://github.com/Planet-DokuShojo/"
+    },
+    {
+        title: "VitaeAggregate",
+        desc: "A community app designed around critiquing and building ATS friendly resumes",
+        tech: ["Cypress", "Node.js", "Svelte", "DjangoRest", "PostgreSQL", "Python", "Typescript", "pytest", "Docker"],
+        image: "/project-captures/Hato.JPG",
+        link: "https://github.com/vitaeaggregate"
+    },
+    {
+        title: "Portfolio Site",
+        desc: "A portfolio showcasing my work",
+        tech: ["Next.js", "Tailwind", "Tailwind"],
+        image: "/project-captures/VitaeAggregate.JPG",
+        link: "https://github.com/Bcook2289/portfolio"
+    },
+
 ];
 
 export default function ProjectsPage() {
