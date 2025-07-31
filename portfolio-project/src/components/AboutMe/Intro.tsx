0cd1ea5fddbd6c0d02349690fd164d15479ba573
Brandon <bcook2289@gmail.com>
2025-07-30 23:19:11 -0400

    feat completed basic styling of about

--- portfolio-project/src/components/AboutMe/Intro.tsx
+++ portfolio-project/src/components/AboutMe/Intro.tsx
@@ -1,4 +1,5 @@
 "use client";
+import {motion} from "framer-motion";
 
 type summaryProp = {
   pageFlag?: "brief" | "detailed";
@@ -7,27 +8,36 @@ type summaryProp = {
 const Intro = ({ pageFlag = "detailed" }: summaryProp) => {
   const detailed = (
     <>
-      <p>
-        I&apos;m a detail-oriented Solutions Consultant and Full-Stack Developer
-        with a background in client operations, analytical problemsolving, and
-        collaborative technical delivery. With over 5 years of experience
-        supporting SaaS platforms in the energy industry, both in Japan and the
-        US, and recent hands-on development work using tools like React,
-        Next.js, TypeScript, Tailwind CSS, and Python/Django, I can bridge the
-        gap between customer needs and technical implementation. My strengths
-        include stakeholder communication, project management, frontend UI/UX
-        design, and backend service integration. I have a strong understanding
-        of Agile and Waterfall methodologies and have managed multiple projects
-        and test plans.
-      </p>
-      <p>
-        I&apos;m also passionate about building intuitive, scalable web
-        applications. The site you&apos;re currently on was built by me utilizing
-        TypeScript, Next.js and Tailwind! I combine my background in technical
-        consulting and client support with a focus on modern full-stack
-        development. I enjoy solving problems at the intersection of business
-        and engineering.
-      </p>
+      <motion.p 
+        className="-translate-x-36"
+        initial={{ x: -200, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ type: "spring", stiffness: 50, delay: 0.2 }}
+        >
+       I&apos;m a detail-oriented Solutions Consultant and Full-Stack Developer 
+       with a background in client operations, collaborative technical delivery, 
+       and analytical problem-solving. I&apos;ve spent over five years supporting 
+       SaaS platforms in the energy industry in both Japan and the US, helping to 
+       bridge the gap between technical teams and customer needs. I&apos;m 
+       experienced with tools like React, Next.js, TypeScript, Tailwind CSS, as  
+       well as Python/Django, and I&apos;ve led projects across both Agile and 
+       Waterfall environments. My strengths include stakeholder communication, 
+       project coordination, backend service integration, and frontend UI/UX design.
+      </motion.p>
+      <motion.p 
+        className="pt-24 translate-x-36"
+        initial={{ x: 200, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ type: "spring", stiffness: 50, delay: 0.2 }}
+      >
+        I&apos;m also passionate about building clean, intuitive web experiences, and 
+        this site is one of them! I designed and developed it using TypeScript, 
+        Next.js, and Tailwind CSS. I love solving real-world problems where 
+        business and engineering meet, and I&apos;m always looking to create tools 
+        that are scalable, user-friendly, and thoughtfully designed. With a 
+        foundation in technical consulting and hands-on dev experience, I bring 
+        a well-rounded, practical approach to every project I work on.
+      </motion.p>
     </>
   );
 
