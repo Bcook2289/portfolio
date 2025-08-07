673be859f934eb31894ae93b0109d78f5a9ba6d6
Brandon <bcook2289@gmail.com>
2025-08-07 11:04:56 +0900

    feat: added framer motion to card compoenents-issue was with use client not being present

--- portfolio-project/src/app/projects/page.tsx
+++ portfolio-project/src/app/projects/page.tsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion";
 import NavBar from "../../components/NavBar";
 import Card from "../../components/Card";
 import { techIcons } from "../../components/utils/techIcons";
@@ -7,24 +6,22 @@ import { projectData } from "../../components/utils/projectData";
 export default function ProjectsPage() {
     return (
         <div className="text-black dark:text-white bg-white dark:bg-black flex flex-col sm:h-screen">
-
             <NavBar/>
             <main className="flex flex-wrap justify-center items-center sm:py-24 flex-grow">
-                {projectData.map((project, index) => (
-                    <div 
-                    key={index} 
-                    className="rounded-2xl sm:h-10/12 shadow-lg overflow-hidden font-sora bg-gray-300 text-black p-4 m-4 w-80"
-                    >
-                        <Card 
-                            title={project.title} 
-                            desc={project.desc}
-                            link={project.link}
-                            image={project.image}
-                            tech={project.tech}
-                            techIcons={techIcons}
-                            />
-                    </div>
-                ))}
+                {projectData.map((project, index) => 
+                    (
+                        <div key={index} className="sm:h-11/12">
+                            <Card    
+                                title={project.title} 
+                                desc={project.desc}
+                                link={project.link}
+                                image={project.image}
+                                tech={project.tech}
+                                techIcons={techIcons}
+                                index={index}
+                                />
+                        </div>
+                    ))}
             </main>
         </div>
         
