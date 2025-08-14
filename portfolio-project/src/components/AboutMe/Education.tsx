3d1c6e018c4704e61452f1017354c01edf59fff8
Brandon <bcook2289@gmail.com>
2025-08-14 16:02:54 +0900

    feat: education and professional experience translated

--- portfolio-project/src/components/AboutMe/Education.tsx
+++ portfolio-project/src/components/AboutMe/Education.tsx
@@ -1,10 +1,22 @@
+"use client"
+
+import { useTranslations } from "next-intl";
 import { educationData } from "../utils/educationData";
+
+interface education {
+    edu: string,
+    school: string,
+    date: string,
+    location: string
+}
 const Education = () => {
+    const t = useTranslations()
+    const educationDataRaw = t.raw("education") as education[];
     return (
         <>
             <section>
                 <h2 className="text-2xl font-semibold mb-4 bg-gray-100 text-center">Educational Background</h2>
-                    {educationData.map((edu, index) => {
+                    {educationDataRaw.map((edu, index) => {
                         return (
                             <div key={index} className="flex mb-4 justify-between">
                                 <h3>{edu.edu}, {edu.school}</h3>
