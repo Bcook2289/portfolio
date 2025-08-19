d9ba860c1011fc4616782fc4d477d99ee4f8c134
Brandon Cook <bcook2289@gmail.com>
2025-08-19 17:14:27 +0900

    feat: MIDDLEWARE COMPLETE

--- portfolio-project/src/app/[locale]/layout.tsx
+++ portfolio-project/src/app/[locale]/layout.tsx
@@ -2,23 +2,14 @@ import { NextIntlClientProvider } from 'next-intl';
 import { notFound } from 'next/navigation';
 import { ReactNode } from 'react';
 
-import en from "../messages/en.json";
-import jp from "../messages/jp.json";
-
-// type LocaleType = "en" | "jp"
-
-// const messagesMap = {en, jp};
-
 export default async function LocalLayout({
   children, 
-  params: { locale }
+  params
 }: {
   children:ReactNode;
-  // params: Promise<{locale: string}>;
-  params: {locale: string};
+  params: Promise<{locale: string}>;
 }) {
-  // const { locale } = await params;
-  // const messages = messagesMap[locale as LocaleType] ?? messagesMap.en;
+  const { locale } = await params;
   let messages;
   try {
     messages = (await import(`../messages/${locale}.json`)).default;
