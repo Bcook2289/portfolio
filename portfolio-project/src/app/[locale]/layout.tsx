51fad130fec5dd5bc8bb516f7a907a475a8b6b67
Brandon Cook <bcook2289@gmail.com>
2025-08-19 10:44:20 +0900

    pushing to main

--- portfolio-project/src/app/[locale]/layout.tsx
+++ portfolio-project/src/app/[locale]/layout.tsx
@@ -1,4 +1,3 @@
-import { getMessages } from 'next-intl/server';
 import { NextIntlClientProvider } from 'next-intl';
 import { notFound } from 'next/navigation';
 import { ReactNode } from 'react';
@@ -6,21 +5,27 @@ import { ReactNode } from 'react';
 import en from "../messages/en.json";
 import jp from "../messages/jp.json";
 
-type LocaleType = "en" | "jp"
+// type LocaleType = "en" | "jp"
 
-const messagesMap = {en, jp};
+// const messagesMap = {en, jp};
 
 export default async function LocalLayout({
   children, 
-  params
+  params: { locale }
 }: {
   children:ReactNode;
-  params: Promise<{locale: string}>;
+  // params: Promise<{locale: string}>;
+  params: {locale: string};
 }) {
-  const { locale } = await params;
-  const messages = messagesMap[locale as LocaleType] ?? messagesMap.en;
+  // const { locale } = await params;
+  // const messages = messagesMap[locale as LocaleType] ?? messagesMap.en;
+  let messages;
+  try {
+    messages = (await import(`../messages/${locale}.json`)).default;
+  } catch (error) {
+    notFound();
+  }
 
-  if(!messages) notFound();
 
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
