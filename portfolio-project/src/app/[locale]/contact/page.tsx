56c0c0648d0f441cd5396ff3af795e7838f45e07
bcook <bcook2289@gmail.com>
2025-09-23 14:50:38 +0900

    feat: simple styling fix

--- portfolio-project/src/app/[locale]/contact/page.tsx
+++ portfolio-project/src/app/[locale]/contact/page.tsx
@@ -3,14 +3,14 @@ import ContactForm from "../../../components/UI/ContactForm";
 import NavBar from "../../../components/UI/NavBar";
 
 export default function contactMePage() {
-    return (
-        <div className="page-container">
-            <NavBar/>
-            <main className="main-container">
-                <div className="m-auto sm:mt-24 lg:w-1/3 p-4 rounded-2xl shadow-md text-xs sm:text-lg bg-gray-300">
-                    <ContactForm/>
-                </div>
-            </main>    
+  return (
+    <div className="page-container">
+      <NavBar />
+      <main className="main-container">
+        <div className="m-16 sm:mt-24 w-2/3 lg:w-1/3 p-4 rounded-2xl shadow-md text-xs sm:text-lg bg-gray-300">
+          <ContactForm />
         </div>
-    )
+      </main>
+    </div>
+  );
 }
